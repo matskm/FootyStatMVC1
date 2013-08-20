4f7c432d73ba0854365bfeaaaabb60d12aa47371
matskm <matskm@gmail.com>
2013-08-20 17:55:47 +0100

    Modify default routing

--- FootyStatMVC1/App_Start/RouteConfig.cs
+++ FootyStatMVC1/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace FootyStatMVC1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "PlayerStat", action = "SelectPlayer", id = UrlParameter.Optional }
             );
         }
     }
