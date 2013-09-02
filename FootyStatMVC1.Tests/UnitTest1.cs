0bb69b4aac4908f3bdeac5b51589f269b7993fa3
matskm <matskm@gmail.com>
2013-09-02 16:31:29 +0100

    Major overhaul prior to v1.0 publish
    A lot of refactoring the framework to get code out of
    PlayerStatController (implmenting the Command Pattern).

---
+++ FootyStatMVC1.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using WatiN.Core;
+
+namespace FootyStatMVC1.Tests
+{
+    [TestFixture]
+    [RequiresSTA]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            //var view = new PlayerStatController.
+            // Open a new Internet Explorer window and
+            // goto the google website.
+            IE ie = new IE("http://www.google.com");
+
+            // Find the search text field and type Watin in it.
+            ie.TextField(Find.ByName("q")).TypeText("WatiN");
+
+            // Click the Google search button.
+            ie.Button(Find.ByValue("Google Search")).Click();
+
+            // Uncomment the following line if you want to close
+            // Internet Explorer and the console window immediately.
+            //ie.Close();
+
+        }
+    }
+}
