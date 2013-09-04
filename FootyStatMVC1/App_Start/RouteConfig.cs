0e386af07313524cb32387da8840a3c0c3fcdf1a
matskm <matskm@gmail.com>
2013-09-04 15:00:51 +0100

    Tweak default route

--- FootyStatMVC1/App_Start/RouteConfig.cs
+++ FootyStatMVC1/App_Start/RouteConfig.cs
@@ -22,7 +22,7 @@ namespace FootyStatMVC1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "PlayerStat", action = "Index", id = UrlParameter.Optional }
                 //defaults: new { controller = "PlayerStat", action = "SelectPlayer", id = UrlParameter.Optional }
             );
         }
